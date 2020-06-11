7787a13e91b9acfd2b6f909b7b43c33411912906
Artem <artemkush1@gmail.com>
2020-06-12 02:16:44 +0300

    fix

--- Task3.m
+++ Task3.m
@@ -2,7 +2,7 @@ clear;
 s = tf ('s');
 g = 1;
 
-alpha = 0.1
+alpha = 0.1;
 q = s;
 % Подборка коэффициента
 calculate(alpha, q, 0, g);
@@ -11,13 +11,13 @@ calculate(alpha, q*alpha, 200, g);
 
 
 function calculate(alpha, q, shift_, g)
-    W_yg = (2*q+1)/(q^2+2*q+1);
+    W_yg = (q^2*18*alpha+3*q^2+3*q+1)/(q+1)^3;
     [y,t] = step(W_yg, 10);
     step_info = stepinfo(W_yg,'SettlingTimeThreshold', 0.05);
     tau_r = step_info.SettlingTime
     t_r = alpha * tau_r
 
-    W = 1/(q^2+2*q+1);
+    W = 1/(q+1)^3;
     [y_table,t] = step(W, 10);
     step_info_table = stepinfo(W,'SettlingTimeThreshold', 0.05);
     tau_r_table = step_info_table.SettlingTime
@@ -32,7 +32,7 @@ function calculate(alpha, q, shift_, g)
     xline(tau_r_table, '-.re', 'Linewidth', 1);
     yline(g - 0.05*g);
     yline(g + 0.05*g);
-    plot(tau_r, 1 + 0.05*1, 'oblack', 'Linewidth', 3);
+    plot(tau_r, 1 - 0.05*1, 'oblack', 'Linewidth', 3);
     plot(tau_r_table, 1 - 0.05*1, 'oblack', 'Linewidth', 3);
 
     xlabel('time (seconds)');
