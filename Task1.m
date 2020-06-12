2b1c360481d64b0d383b1a3ddcd5dee887a4c6fe
Artem <artemkush1@gmail.com>
2020-06-12 14:44:53 +0300

    fix

--- Task1.m
+++ Task1.m
@@ -1,36 +1,37 @@
 clear;
 t = 0:0.01:10;
-s = tf('s');
 
-% Дано 
-A = 4
+
+s = tf('s');
+% Передаточная функция
+Ws = (1 / (s ^ 2 + s + 1));
+
+
+A = 4;
 w = 5;
-alpha = 0
+alpha = 0;
 u = A*sin(w*t + alpha);
 
 % Передаточная функция
-W = 3 / (-(w)^2 + 2*w*1i + 4);
+W = 3 / ((w*1i)^2 + 2*w*1i + 4);
+Aw = abs(W);
+FIw = -pi + atan(imag(W)/real(W)) % fi
+
 
 % График реакции системы в формате S
 figure('position', [550, 150, 1200, 600]);
 step(tf([3],[1,2,4]),10);
 title('System reaction');
 
-B = round(abs(W) * A, 2) % Амплитуда выходного сигнала
-betta = round(alpha + angle(W), 2) % Фаза выходного сигнала
+B = round(Aw * A, 2) % Амплитуда выходного сигнала
 
-y = B * sin(w*t + betta); % Новый сигнал
+betta = round(alpha + angle(W), 2); % Фаза выходного сигнала
 
-
-% Для построения графика: ищу смещение, дабы не выйти за пределы figure
-n = 0;
-while (2*pi*n - min(alpha, betta) + pi/2) / w < 0
-    n = n + 1;
-end
+y = lsim(Ws, u, t);
 
 % Экстремумы
-pick_u = (pi/2 - alpha + 2*pi*n)/w;
-pick_y = (pi/2 - betta + 2*pi*n)/w;
+pick_u = (pi/2 - alpha)/w;
+pick_y = (pi/2 - betta)/w;
 
 % Относительное смещение пиков.
 shift_x = [pick_u, pick_y];
@@ -42,7 +43,7 @@ figure('position', [250, 250, 1000, 700]);
 p = plot(t, u, t, y, shift_x, shift_y);
 p(1).LineWidth = 2;
 p(2).LineWidth = 2;
-p(3).LineWidth = 6;
+p(3).LineWidth = 4;
 grid on;
 
 % Вертикальные линии
@@ -56,4 +57,4 @@ xl2.LabelHorizontalAlignment = 'center';
 % Подписи
 xlabel('time (seconds)');
 ylabel('4sin5t');
-legend('Asin(wt+alpha)', 'Bsin(wt+betta)', 'Shift');
+legend('Asin(wt+alpha)', 'System reaction', 'Shift');
