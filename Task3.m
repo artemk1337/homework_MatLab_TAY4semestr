73c890e855c343c617fb91d9a14c7061d3b2034c
Artem <artemkush1@gmail.com>
2020-06-12 21:14:46 +0300

    fix

--- Task3.m
+++ Task3.m
@@ -11,14 +11,15 @@ calculate(alpha, q*alpha, 200, g);
 
 
 function calculate(alpha, q, shift_, g)
+    t = (0:0.01:10)';
     W_yg = (q^2*18*alpha+3*q^2+3*q+1)/(q+1)^3;
-    [y,t] = step(W_yg, 10);
+    [y,t] = step(W_yg, t);
     step_info = stepinfo(W_yg,'SettlingTimeThreshold', 0.05);
     tau_r = step_info.SettlingTime
     t_r = alpha * tau_r
 
     W = 1/(q+1)^3;
-    [y_table,t] = step(W, 10);
+    [y_table,t] = step(W, t);
     step_info_table = stepinfo(W,'SettlingTimeThreshold', 0.05);
     tau_r_table = step_info_table.SettlingTime
     t_r_table = alpha * tau_r_table
